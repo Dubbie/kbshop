b2f7be7e675c6d502621c40d3a8deda8fb637462
Mihó Dániel <miho.daniel@mdy.hu>
2024-05-16 13:03:32 +0200

    add roles

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
 {
@@ -14,10 +15,14 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         // Create admin user
-        User::create([
+        $superAdmin = Role::create(['name' => 'Super Admin']);
+
+        $admin = User::create([
             'name' => config('admin.name'),
             'email' => config('admin.email'),
             'password' => Hash::make(config('admin.password')),
         ]);
+
+        $admin->assignRole($superAdmin);
     }
 }
