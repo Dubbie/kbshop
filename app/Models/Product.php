67ade5e633871ca563b1ed018826cf56054226e0
Mihó Dániel <miho.daniel@mdy.hu>
2024-05-28 16:36:22 +0200

    remove unused code

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -19,10 +19,4 @@ class Product extends Model
     {
         return $this->hasMany(Sku::class);
     }
-
-    public function scopeBaseProducts($query)
-    {
-        // Count the related skus, and only return products with 1 sku
-        return $query->where('type', 'product')->withCount('skus')->having('skus_count', '=', 1);
-    }
 }
